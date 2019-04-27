200deab9f1ac59a6ed1b4ec9ff9416d6454eb9b0
Jordan Hall <Jordan@libertyware.co.uk>
2019-04-27 17:04:32 +0100

    Develop (#5)
    * ci: add automerge
    
    * ci: add eslint rules (#4)
    
    * ci: added commit linting and pre-commit hooks (#1)
    
    * fix: ES lint rules (#6)

---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional', '@commitlint/config-angular'],
+  rules: {
+    'subject-max-length': [2, 'always', 120],
+    'scope-enum': [2, 'always', []],
+    'scope-empty': [2, 'always'],
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'release', 'revert', 'style', 'test']
+    ]
+  }
+};
