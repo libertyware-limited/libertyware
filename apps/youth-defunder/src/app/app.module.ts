bd774ad68dd59fe0777ecaea58e2a421e5f03a12
Jordan Hall <Jordan@libertyware.co.uk>
2019-04-28 20:16:19 +0100

    feat: ng-zorro by ants added (#9)
    * feat: ng-zero by ants added
    
    * fix: fix missing alt tag
    
    * test: remove unit test as it will not be unsed eventually

--- apps/youth-defunder/src/app/app.module.ts
+++ apps/youth-defunder/src/app/app.module.ts
@@ -5,6 +5,14 @@ import { RouterModule } from '@angular/router';
 import { NgSentryModule } from '@libertyware/angular/ng-sentry';
 
 import { AppComponent } from './app.component';
+import { NgZorroAntdModule, NZ_I18N, en_GB } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,9 +21,13 @@ import { AppComponent } from './app.component';
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
     NgSentryModule.forRoot({
       dsn: 'https://58ffce333b21422483b37bae9484f34a@sentry.io/1447726'
-    })
+    }),
+    NgZorroAntdModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_GB }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
